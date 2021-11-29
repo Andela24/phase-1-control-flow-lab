0a1840d4e5a7b597692260785e0de67de4d14fe1
Andela Stevanovic <andjela.s166@gmail.com>
2021-11-29 16:48:29 -0500

    commit tree d4369cac6cd6724df461d5f85772d366f3640f96

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue) {
   // Write your code here!
+  let result
+if (someValue <= 400) {
+  result = "This one is on me!";
+return result;
+}
+ else if (someValue <= 2499 && someValue >= 2000) {
+result = "I will gladly take your thirty bucks."
+return result;
+ }
+ else if(someValue >= 2500) {
+   result = "No can do."
+   return result;
+ }
+ }
+
+function ternaryCheckCity(city){
+  // Write your code here!
+ const cityMessage = (city === 'NYC') ? ('Ok, sounds good.') : ('No go.');
+ 
+ return cityMessage;
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
+  switch(tip) {
+  case 'generous':
+    return 'Thank you so much.';
+    break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+      default:
+        return 'Bye.';
+        break;
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+tip;
 }
